dc4a7733bf24e7fc562abe3c5bd5609aef140837
やむぅ。です <95989535+yamu-studio@users.noreply.github.com>
2024-11-22 12:47:48 +0900

    -add バリデーションを追加

--- nuxt_app/pages/index.vue
+++ nuxt_app/pages/index.vue
@@ -2,6 +2,10 @@
 .section
   .container.is-max-tablet
     .box
+      .block.notification.is-danger.scrollView(v-if="onErrorMessage && v$.$invalid")
+        button.delete(@click="closeErrorMessage")
+        p.help(v-for="error in v$.$silentErrors" :key="error.key") {{ error.$message }}
+  
       .field
         label.label お名前
           span(style="color: red") *
@@ -53,11 +57,14 @@
             | No
 
       .buttons
-        button.button.is-primary(@click="sencContact") 送信する
+        button.button.is-primary(@click="checkForm") 送信する
         button.button.is-link.is-light Cancel
 </template>
 
 <script setup lang="ts">
+import { useVuelidate } from "@vuelidate/core";
+import { required, maxLength, helpers } from "@vuelidate/validators";
+
 // ＝＝＝＝＝＝＝＝＝＝＝＝ 入力されたデータの保管 ＝＝＝＝＝＝＝＝＝＝＝＝＝
 const formData = ref({
   name: "",
@@ -68,6 +75,49 @@ const formData = ref({
   isConsent: false,
   isAllowDelay: false,
 });
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝ バリデーション系 ＝＝＝＝＝＝＝＝＝＝＝＝＝
+const requireText = "必須項目です。";
+const formRule = {
+  name: {
+    required: helpers.withMessage("お名前：" + requireText, required),
+    maxLength: helpers.withMessage(
+      "お名前：1~10文字でお願いします。",
+      maxLength(10)
+    ),
+  },
+  mailAddress: {
+    required: helpers.withMessage("メールアドレス：" + requireText, required),
+  },
+  contentDetail: {
+    required: helpers.withMessage("お問い合わせ詳細：" + requireText, required),
+  },
+};
+const v$ = useVuelidate(formRule, formData);
+
+const onErrorMessage = ref(false);
+function closeErrorMessage() {
+  onErrorMessage.value = false;
+}
+
+function checkForm() {
+  if (v$.value.$invalid) {
+    onErrorMessage.value = true;
+    return;
+  }
+
+  if (!formData.value.isConsent) {
+    alert("プライバシーポリシーに同意しない場合は送信できません。");
+    return;
+  }
+  if (!formData.value.isAllowDelay) {
+    alert("もひとつ確認に同意しない場合は送信できません。");
+    return;
+  }
+
+  // OKなら送信
+  sencContact();
+}
 
 function sencContact() {
   alert("送信しました！");
